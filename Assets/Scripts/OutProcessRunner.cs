45d19b3cec9b09f673c084f9734f4cc1ccb75b05
Bakeneko <67040613+Bakeneko-git@users.noreply.github.com>
2023-07-22 00:32:03 +0900

    Update OutProcessRunner.cs

--- Assets/Scripts/OutProcessRunner.cs
+++ Assets/Scripts/OutProcessRunner.cs
@@ -8,7 +8,7 @@ using UnityEngine;
 public class OutProcessRunner : MonoBehaviour
 {
     private Process process;
-    public string pyfile = @"C:\Users\Haruya\Documents\UnityProject\OutProcessTest\printout.py";
+    public string pyfile = @".\printout.py";
     // Start is called before the first frame update
     void Start()
     {
